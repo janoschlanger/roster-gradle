552b88a62036ca54a0f0abd7fcec6869076d30e7
Janosch Langer <janosch.langer@cgi.com>
2017-06-07 11:14:35 -0600

    -

--- src/main/java/org/cloudfoundry/Application.java
+++ src/main/java/org/cloudfoundry/Application.java
@@ -2,10 +2,11 @@ package org.cloudfoundry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
